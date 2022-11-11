f65114858b1cd49e07d9d32e52ff3c4ce8fdb5b7
nahuel echeto <echetonahuel@gmail.com>
2022-11-10 21:42:53 -0300

    instalacion de bootstrap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,11 +5,12 @@ import { Link } from "react-router-dom";
 function NavBar () {
     return (
         <div className="div-navBar">
-            <Link to="/" className="logo"><img src="/img/logo.jpg" alt="logo" /></Link>
+            <Link to="/" className="logo"><img src="/img/logoHamburguesa.png" alt="logo" /></Link>
             <div className="div-link">
-                <Link to="/">Cerraduras</Link>
-                <Link to="/">Bisagras</Link>
-                <Link to="/detalle">Detalle</Link>
+                <Link to="/">Hamburguesas</Link>
+                <Link to="/">Guarniciones</Link>
+                <Link to="/detalle">Bebida</Link>
+                <Link to="/">Postres</Link>
             </div>
             <Link to="/" className="img-carrito"><CartWidget /></Link>
         </div>
